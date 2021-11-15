186ea5c2f6f6549286a9764cda6dc07ca19bf9c3
alberto <alyoung1991@gmail.com>
2021-11-14 23:57:11 -0500

    Finished Project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -12,12 +13,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
